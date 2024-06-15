7029b8f554f31f5f78921c542c3dfbc146b09ceb
gurayerus <gurayerus@gmail.com>
2024-06-15 11:56:27 -0400

    Edit

--- utils/util_combat_test.sh
+++ utils/util_combat_test.sh
@@ -6,6 +6,6 @@ in_mdl=$2
 out_csv=$3
 
 ## Apply combat learn
-cmd="neuroharm -a apply -i $in_csv -m $in_mdl -o $out_csv"
+cmd="neuroharm -a apply -i $in_csv -m $in_mdl -u $out_csv"
 echo "About to run: $cmd"
 $cmd
