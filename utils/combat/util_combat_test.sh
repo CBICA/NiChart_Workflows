7bb0a31f0a8be1a1f000840a309ff411b057da7d
gurayerus <gurayerus@gmail.com>
2024-08-12 14:40:28 -0400

    Edit

--- utils/combat/util_combat_test.sh
+++ utils/combat/util_combat_test.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 ## Read input
-in_csv=$1
-in_mdl=$2
-out_csv=$3
+in_csv=$(realpath $1)
+in_mdl=$(realpath $2)
+out_csv=$(realpath -m $3)
 
 ## Apply combat learn
 cmd="neuroharm -a apply -i $in_csv -m $in_mdl -u ${out_csv%.csv}_tmpinit.csv"
